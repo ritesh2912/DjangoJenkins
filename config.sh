10184a72123fe7cc8674fc8b7b73b3b360987984
ritesh2912 <94890713+ritesh2912@users.noreply.github.com>
2022-05-10 13:07:01 +0530

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,54 @@
+#!/bin/bash 
+
+deactivate
+
+echo "[Unit]
+Description=gunicorn socket
+[Socket]
+ListenStream=/run/gunicorn.sock
+[Install]
+WantedBy=sockets.target
+" > /etc/systemd/system/gunicorn.socket
+
+echo " [Unit]
+Description=gunicorn daemon
+Requires=gunicorn.socket
+After=network.target
+[Service]
+User=ubuntu
+Group=www-data
+WorkingDirectory=/home/ubuntu/projectdir/projectdir
+ExecStart=/home/ubuntu/env/bin/gunicorn \
+          --access-logfile - \
+          --workers 3 \
+          --bind unix:/run/gunicorn.sock \
+          first.wsgi:application
+[Install]
+WantedBy=multi-user.target"  >  /etc/systemd/system/gunicorn.service
+
+sudo systemctl start gunicorn.socket
+
+sudo systemctl enable gunicorn.socket
+
+
+echo "server {
+    listen 80;
+    server_name 13.233.101.132;
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location /static/ {
+        root /home/ubuntu/projectdir;
+    }
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/run/gunicorn.sock;
+    }
+}" > /etc/nginx/sites-available/textutils
+
+
+sudo ln -s /etc/nginx/sites-available/textutils  /etc/nginx/sites-enabled/
+
+sudo rm /etc/nginx/sites-enabled/default
+
+
+sudo systemctl restart nginx
+sudo systemctl restart gunicorn
